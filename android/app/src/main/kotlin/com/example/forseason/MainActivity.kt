d1d1f2c1c66b9b3739ad95d0a5b60c539a3afa3b
imseongsu <tjdtn2323@naver.com>
2021-07-05 11:53:25 +0900

    package name change -> twoguys

--- android/app/src/main/kotlin/com/example/forseason/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.forseason
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/twoguys/forseason/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.twoguys.forseason
+
+import io.flutter.embedding.android.FlutterActivity
+// import 3개
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+class MainActivity: FlutterActivity() {
+
+
+
+}
